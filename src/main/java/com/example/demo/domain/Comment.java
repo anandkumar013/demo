36db523cbc6a46cbb1a7de5ccf5b2287177c128c
Anand <anand.kumar@thoughtworks.com>
2021-02-26 03:31:23 +0530

    created entities class and refracted using project lambok

---
+++ src/main/java/com/example/demo/domain/Comment.java
@@ -0,0 +1,24 @@
+package com.example.demo.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private  long id;
+    private  String body ;
+
+    //link
+
+
+}
---
+++ src/main/java/com/example/demo/domain/Link.java
@@ -0,0 +1,27 @@
+package com.example.demo.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Link {
+
+    @Id
+    @GeneratedValue
+    private  long id;
+    @NonNull
+    private  String title ;
+    @NonNull
+    private String url ;
+
+    //comments
+
+
+}
