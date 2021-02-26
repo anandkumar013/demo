707f28ce549afdc7283c134a8abebc0d3c5a00e9
Anand <anand.kumar@thoughtworks.com>
2021-02-26 17:18:15 +0530

    link controller

---
+++ src/main/java/com/example/demo/controller/LinkController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.Link;
+import com.example.demo.repository.LinkRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/links")
+public class LinkController {
+
+    private LinkRepository linkRepository;
+
+    public LinkController(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    // list
+    @GetMapping("/")
+    public List<Link> list() {
+        return linkRepository.findAll();
+    }
+
+    // CRUD
+    @PostMapping("/create")
+    public Link create(@ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+    @GetMapping("{/id}")
+    public Optional<Link> read(@PathVariable long id)  {
+        return linkRepository.findById(id);
+    }
+
+    @PutMapping("{/id}")
+    public Link update(@PathVariable long id,@ModelAttribute Link link) {
+        // get id
+        return linkRepository.save(link);
+    }
+    @DeleteMapping("{/id}")
+    public void delete(@PathVariable long id) {
+         linkRepository.deleteById(id);
+    }
+}
